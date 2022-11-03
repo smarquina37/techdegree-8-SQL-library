ea41b549a6805a2135eb87fe299b861f9c4bc541
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-11-03 12:34:38 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const { Book } = require("../models/book.js");
 
 /* GET home page. */
 router.get("/books", async (req, res, next) => {
-  // res.render("index", { title: "Express" });
   const books = await Book.findAll({});
   console.log(books);
 });
