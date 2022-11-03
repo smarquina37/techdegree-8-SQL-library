05a64e73c84511a832fb713a7e8aae5bcbeb499a
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-11-03 10:03:16 -0700

    Initial Commit

---
+++ models/book.js
@@ -0,0 +1,40 @@
+"use strict";
+const Sequelize = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Sequelize.Model {}
+  Book.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "title"',
+          },
+          notEmpty: {
+            msg: 'Please provide a value for "title"',
+          },
+        },
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "author"',
+          },
+          notEmpty: {
+            msg: 'Please provide a value for "title"',
+          },
+        },
+      },
+      genre: DataTypes.STRING,
+      year: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Book",
+    }
+  );
+  return Book;
+};
