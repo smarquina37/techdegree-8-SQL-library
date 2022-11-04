392fc44dcef3de4c2378f6e59fcecdaafd2e9cc6
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-11-03 18:05:47 -0700

    Update asyncHandler in index.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -31,7 +30,7 @@ const sequelize = new Sequelize({
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -40,14 +39,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handlers
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,31 @@ const express = require("express");
 const router = express.Router();
 const { Book } = require("../models/book.js");
 
+// async Handler -  middleware to wrap each of our routes automatically in a try-catch block
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      // res.render("error", { error: err });
+      next(error);
+    }
+  };
+}
+
 /* GET home page. */
-router.get("/books", async (req, res, next) => {
-  const books = await Book.findAll({});
-  console.log(books.map((book) => book.toJSON));
+// redirect '/' to '/books'
+router.get("/", (req, res) => {
+  res.redirect("/books");
 });
+
+router.get(
+  "/books",
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    res.render("/book", { title: "Books", books: books.books });
+    console.log(books);
+  })
+);
 
 module.exports = router;
